b54cbbbff13587f4ee54fcf57468b69d4300a401
parkjongmin <jmpak99@naver.com>
2024-10-26 16:08:53 +0900

    ✨ Feat : Profile Setting

--- coplate/views.py
+++ coplate/views.py
@@ -14,7 +14,7 @@ from braces.views import LoginRequiredMixin, UserPassesTestMixin
 from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordChangeView
 from coplate.models import User, Review
-from coplate.forms import ReviewForm
+from coplate.forms import ReviewForm, ProfileForm
 from coplate.functions import confirmation_required_redirect
 
 # Create your views here.
@@ -112,6 +112,19 @@ class UserReviewListView(ListView):
         context = super().get_context_data(**kwargs)
         context["profile_user"] = get_object_or_404(User, id = self.kwargs.get("user_id"))
         return context
+    
+class ProfileSetView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileForm
+    template_name = "coplate/profile_set_form.html"
+
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+    def get_success_url(self):
+        return reverse("index")
+
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
